e4e46cf6a7f5f1bba05fbb48893b206836879ce9
natekhchan <natekhchan@gmail.com>
2024-09-02 22:29:50 -0700

    antd commit 3rd time final

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Import your custom styles (if any)
-import './antd/dist/reset.css'; // Import Ant Design styles
+import 'antd/dist/reset.css'; // Import Ant Design styles
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
